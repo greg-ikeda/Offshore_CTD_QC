d9fa4270f2bd60d5c53a5e8da6eef05b9e3d853f
greg-ikeda <gregikeda@gmail.com>
2023-06-27 11:17:46 -0700

    Commiting an older version from 6/26! We'll see what happens.

--- Offshore_CTD_RangeTest.R
+++ Offshore_CTD_RangeTest.R
@@ -9,7 +9,7 @@ library(cmocean)
 library(RColorBrewer)
 library(here)
 library(plotly)
-source(paste0(here(), "/", "contour_functions.R"))
+source(here("contour_functions.R"))
 
 starttime <- Sys.time()
 
@@ -27,22 +27,13 @@ station <- "KSBP01"
 
 # # Where do you want to save output?
 save_folder <- here("output")
-folder <- paste0("C:\\Users\\gikeda\\R\\Offshore_CTD_QC\\",
-                 station, "\\")
-fname <- list.files(folder, pattern = "_qcd.csv")
+# folder <- paste0("C:\\Users\\gikeda\\R\\Offshore_CTD_QC\\",
+#                  station, "\\")
+# fname <- list.files(folder, pattern = "_qcd.csv")
 
-test_data <- "C:\\Users\\gikeda\\R\\Offshore_CTD_QC\\data\\KSBP01_Example.csv"
+test_data <- here("data", "KSBP01_Example.csv")
 
 # Load data and calculate baseline/Standard deviations --------------------------
-
-# CTDdata <- import_CTD(paste0(folder, fname))
-CTDdata <- import_CTD(test_data) %>% 
-  mutate(Year = year(Sampledate), 
-         Month = month(Sampledate), 
-         Day = day(Sampledate), 
-         YearDay = yday(Sampledate),
-         Date = as.Date(Sampledate))
-tz(CTDdata$Sampledate) <- "America/Los_Angeles"
 
 badquals <- c("E", "TA, E", "E, Rej", "E,Rej", "E, rej", "R, E", "E, TA",
               "rej", "Rej", "REJ", "R", "Rej, E, TA", "E, TA, rej", "R, TA", "Rej, E",
@@ -52,21 +43,26 @@ goodqual <- "TA"
 
 bin_width <- 0.5
 
+# CTDdata <- import_CTD(paste0(folder, fname))
+CTDdata <- import_CTD(test_data) %>% 
+  mutate(Year = year(Sampledate), 
+         Month = month(Sampledate), 
+         Day = day(Sampledate), 
+         YearDay = yday(Sampledate),
+         Date = as.Date(Sampledate), 
+         Bin = depth_bin(Depth, bin_width), 
+         BinDepth = sapply(Bin, get_bin_depth))
+tz(CTDdata$Sampledate) <- "America/Los_Angeles"
+
+# Create CTDdata_flagged for baseline calculation WITHOUT previously-flagged bad data
 CTDdata_flagged <- CTDdata %>% 
-  mutate(Bin = depth_bin(Depth, bin_width), 
-         BinDepth = sapply(Bin, get_bin_depth), 
-         # Year = year(Sampledate), 
-         # Month = month(Sampledate), 
-         # Day = day(Sampledate), 
-         # YearDay = yday(Sampledate), 
-         Chlorophyll = ifelse(Chlorophyll_Qual %in% badquals, NA, Chlorophyll), 
+  mutate(Chlorophyll = ifelse(Chlorophyll_Qual %in% badquals, NA, Chlorophyll), 
          Density = ifelse(Density_Qual %in% badquals, NA, Density), 
          DO = ifelse(DO_Qual %in% badquals, NA, DO), 
          SigmaT = ifelse(SigmaTheta_Qual %in% badquals, NA, SigmaTheta), 
          Salinity = ifelse(Salinity_Qual %in% badquals, NA, Salinity), 
          Temperature = ifelse(Temperature_Qual %in% badquals, NA, Temperature), 
          NO23 = ifelse(NO23_Qual %in% badquals, NA, NO23))
-tz(CTDdata$Sampledate) <- "America/Los_Angeles"
 
 # Creates a baseline from the first sample in the dataset to the prior year (e.g. 1998 - 2022 for a profile in 2023)
 base_start <- min(CTDdata_flagged$Year)
@@ -75,66 +71,45 @@ base_end <- max(CTDdata_flagged$Year) - 1
 # Similar to contour script - 
 # creates baseline data, calculates baseline data, then adds those columns to the working df "working_data"
 baseline_data <- CTDdata_flagged %>% 
-  select(Sampledate,
+  select(Date,
+         Month, 
+         Year, 
          Depth,
-         Density,
-         Density_Qual,
-         DO,
-         DO_Qual,
-         SigmaTheta,
-         SigmaTheta_Qual,
-         Salinity,
-         Salinity_Qual,
-         Temperature,
-         Temperature_Qual,
          BinDepth,
+         Density,
+         DO,
+         SigmaTheta,
+         Salinity,
+         Temperature,
          Chlorophyll,
-         Chlorophyll_Qual,
          Light_Transmission,
-         Light_Qual,
          PAR,
-         PAR_Qual,
          Surface_PAR,
-         Surface_PAR_Qual,
          Turbidity,
-         Turbidity_Qual,
          NO23,
-         NO23_Qual,
-         Month) %>% 
-  mutate(Year = year(Sampledate)) %>% 
+         Density_Qual,
+         DO_Qual,
+         SigmaTheta_Qual,
+         Salinity_Qual,
+         Temperature_Qual,
+         Chlorophyll_Qual,
+         Light_Qual,
+         PAR_Qual,
+         Surface_PAR_Qual,
+         Turbidity_Qual,
+         NO23_Qual) %>% 
   filter(Year >= base_start, 
          Year <= base_end)
 
 baseline <- baseline_data %>% 
   group_by(Month, BinDepth) %>% 
-  summarize(BaselineDensity = mean(Density, na.rm = T), 
-            BaselineDO = mean(DO, na.rm = T), 
-            BaselineSigmaT = mean(SigmaTheta, na.rm = T), 
-            BaselineSalinity = mean(Salinity, na.rm = T), 
-            BaselineTemperature = mean(Temperature, na.rm = T),
-            BaselineChlorophyll = mean(Chlorophyll, na.rm = T),
-            BaselineSigmaTheta = mean(SigmaTheta, na.rm = T),
-            BaselineLight_Transmission = mean(Light_Transmission, na.rm = T),
-            BaselinePAR = mean(PAR, na.rm = T),
-            BaselineSurface_PAR = mean(Surface_PAR, na.rm = T),
-            BaselineTurbidity = mean(Turbidity, na.rm = T),
-            BaselineNO23 = mean(NO23, na.rm = T),
-            SD_Density = sd(Density, na.rm = T), 
-            SD_DO = sd(DO, na.rm = T), 
-            SD_SigmaT = sd(SigmaTheta, na.rm = T), 
-            SD_Salinity = sd(Salinity, na.rm = T), 
-            SD_Temperature = sd(Temperature, na.rm = T),
-            SD_Chlorophyll = sd(Chlorophyll, na.rm = T),
-            SD_SigmaTheta = sd(SigmaTheta, na.rm = T),
-            SD_Light_Transmission = sd(Light_Transmission, na.rm = T),
-            SD_PAR = sd(PAR, na.rm = T),
-            SD_Surface_PAR = sd(Surface_PAR, na.rm = T),
-            SD_Turbidity = sd(Turbidity, na.rm = T),
-            SD_NO23 = sd(NO23, na.rm = T),
-            n_samples = n())
+  summarize(across(Density:NO23, 
+                   list(mean = ~ mean(.x, na.rm = TRUE), 
+                        sd = ~ sd(.x, na.rm = TRUE), 
+                        n = ~ n())))
 
 # This df has all of the data that we will be working with - baselines, standard deviations, flags
-working_data <- full_join(CTDdata_flagged, baseline)
+working_data <- full_join(CTDdata, baseline)
 
 # Flagging based on extreme values ----------------------------------------
 
@@ -197,48 +172,48 @@ ggplotly(ggplot(extreme_df)+
            geom_bar(aes(x = flag_reason))+
            theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)))
 
-write_csv(extreme_df, paste0(save_folder, "\\Extreme_values.csv"))
+write_csv(extreme_df, paste0(save_folder, "/Extreme_values.csv"))
 
 # Flagging based on standard deviation range ------------------------------
 
 stnddev_df <- working_data %>%
   mutate(
     Chlorophyll_Qual_Auto = case_when(
-      Chlorophyll < (BaselineChlorophyll - 2*SD_Chlorophyll) ~ "q",
-      Chlorophyll > (BaselineChlorophyll + 2*SD_Chlorophyll) ~ "q"),
+      Chlorophyll < (Chlorophyll_mean - 2*Chlorophyll_sd) ~ "q",
+      Chlorophyll > (Chlorophyll_mean + 2*Chlorophyll_sd) ~ "q"),
     Density_Qual_Auto = case_when(
-      Density < (BaselineDensity - 2*SD_Density) ~ "q",
-      Density > (BaselineDensity + 2*SD_Density) ~ "q"),
+      Density < (Density_mean - 2*Density_sd) ~ "q",
+      Density > (Density_mean + 2*Density_sd) ~ "q"),
     DO_Qual_Auto = case_when(
-      DO < (BaselineDO - 2*SD_DO) ~ "q",
-      DO > (BaselineDO + 2*SD_DO) ~ "q"),
+      DO < (DO_mean - 2*DO_sd) ~ "q",
+      DO > (DO_mean + 2*DO_sd) ~ "q"),
     # SigmaTheta_Qual_Auto = case_when(
     #   DO < (BaselineSigmaTheta - 2*SD_SigmaTheta) ~ "q",
     #   DO > (BaselineSigmaTheta + 2*SD_SigmaTheta) ~ "q"),
     Light_Transmission_Qual_Auto = case_when(
-      Light_Transmission < (BaselineLight_Transmission - 2*SD_Light_Transmission) ~ "q",
-      Light_Transmission > (BaselineLight_Transmission + 2*SD_Light_Transmission) ~ "q"),
+      Light_Transmission < (Light_Transmission_mean - 2*Light_Transmission_sd) ~ "q",
+      Light_Transmission > (Light_Transmission_mean + 2*Light_Transmission_sd) ~ "q"),
     PAR_Qual_Auto = case_when(
-      PAR < (BaselinePAR - 2*SD_PAR) ~ "q",
-      PAR > (BaselinePAR + 2*SD_PAR) ~ "q"),
+      PAR < (PAR_mean - 2*PAR_sd) ~ "q",
+      PAR > (PAR_mean + 2*PAR_sd) ~ "q"),
     Surface_PAR_Qual_Auto = case_when(
-      Surface_PAR < (BaselineSurface_PAR - 2*SD_Surface_PAR) ~ "q",
-      Surface_PAR > (BaselineSurface_PAR + 2*SD_Surface_PAR) ~ "q"),
+      Surface_PAR < (Surface_PAR_mean - 2*Surface_PAR_sd) ~ "q",
+      Surface_PAR > (Surface_PAR_mean + 2*Surface_PAR_sd) ~ "q"),
     Salinity_Qual_Auto = case_when(
-      Salinity < (BaselineSalinity - 2*SD_Salinity) ~ "q",
-      Salinity > (BaselineSalinity + 2*SD_Salinity) ~ "q"),
+      Salinity < (Salinity_mean - 2*Salinity_sd) ~ "q",
+      Salinity > (Salinity_mean + 2*Salinity_sd) ~ "q"),
     Temperature_Qual_Auto = case_when(
-      Temperature < (BaselineTemperature - 2*SD_Temperature) ~ "q",
-      Temperature > (BaselineTemperature + 2*SD_Temperature) ~ "q"),
+      Temperature < (Temperature_mean - 2*Temperature_sd) ~ "q",
+      Temperature > (Temperature_mean + 2*Temperature_sd) ~ "q"),
     Turbidity_Qual_Auto = case_when(
-      Turbidity < (BaselineTurbidity - 2*SD_Turbidity) ~ "q",
-      Turbidity > (BaselineTurbidity + 2*SD_Turbidity) ~ "q"),
+      Turbidity < (Turbidity_mean - 2*Turbidity_sd) ~ "q",
+      Turbidity > (Turbidity_mean + 2*Turbidity_sd) ~ "q"),
     NO23_Qual_Auto = case_when(
-      NO23 < (BaselineNO23 - 2*SD_NO23) ~ "q",
-      NO23 > (BaselineNO23 + 2*SD_NO23) ~ "q"),
+      NO23 < (NO23_mean - 2*NO23_sd) ~ "q",
+      NO23 > (NO23_mean + 2*NO23_sd) ~ "q"),
     SigmaT_Qual_Auto = case_when(
-      SigmaT < (BaselineSigmaT - 5*SD_SigmaT) ~ "q",
-      SigmaT > (BaselineSigmaT + 5*SD_SigmaT) ~ "q")) %>%
+      SigmaTheta < (SigmaTheta_mean - 5*SigmaTheta_sd) ~ "q",
+      SigmaTheta > (SigmaTheta_mean + 5*SigmaTheta_sd) ~ "q")) %>%
   mutate(flag_reason = "",
          flag_reason = if_else(!is.na(Chlorophyll_Qual_Auto), paste0(flag_reason, "chl_"), flag_reason),
          flag_reason = if_else(!is.na(Density_Qual_Auto), paste0(flag_reason, "density_"), flag_reason),
@@ -260,7 +235,7 @@ ggplotly(ggplot(stnddev_df)+
   geom_bar(aes(x = flag_reason))+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)))
 
-write_csv(stnddev_df, paste0(save_folder, "\\standard_deviation_values.csv"))
+write_csv(stnddev_df, paste0(save_folder, "/standard_deviation_values.csv"))
 shell.exec(save_folder)
 
 
@@ -271,16 +246,16 @@ test <- baseline %>%
 
 test_plot <- ggplot(test)+
   geom_path(aes(y = BinDepth,
-                x = BaselineDO))+
+                x = DO_mean))+
   scale_y_reverse()+
   geom_path(aes(y = BinDepth,
-                x = BaselineDO + SD_DO),
+                x = DO_mean + DO_sd),
             color = "blue")+
   geom_path(aes(y = BinDepth,
-                x = BaselineDO - SD_DO),
+                x = DO_mean - DO_sd),
             color = "blue")+
   geom_polygon(aes(y = BinDepth,
-                   x = BaselineDO - SD_DO))
+                   x = DO_mean - DO_sd))
 
 plotly::ggplotly(test_plot)
 
@@ -293,16 +268,21 @@ endtime <- Sys.time()
 
 test_plot <- ggplot(test)+
   geom_path(aes(y = BinDepth,
-                x = BaselineDO))+
+                x = DO_mean))+
   scale_y_reverse()
 
 test_plot2 <- ggplot(test)+
   geom_ribbon(aes(x = BinDepth,
                    y = BaselineDO))+
   geom_line(aes(x = BinDepth,
-                y = BaselineDO))+
+                y = DO_mean))+
   scale_x_reverse()+
   coord_flip()+
+<<<<<<< HEAD
+=======
+  geom_polygon(aes(x = BinDepth,
+                   y = DO_mean))
+>>>>>>> dc57d484f10eea7de6633c94dda7906b32730ad4
 
 
 # autoqual_fields <- c(
